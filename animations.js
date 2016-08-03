7b3f78a30d3439d20da506c14738112b18709c2a
rojale <j08zhang24@gmail.com>
2016-08-02 23:43:15 -0400

    publishing

--- animations.js
+++ animations.js
@@ -20,19 +20,35 @@ $(document).ready(function(){
 	function(){
 		$(this).stop().animate({'backgroundColor':'#2c3e50'},150);
 	})
-	$('#engcontentwrapper').hover(
+	$('#engiconwrapper').hover(
 		function(){
 			$('#engcloak').fadeTo(300,1);
 			$('#engcontentwrapper h1').animate({'backgroundColor':'#3498db'},1000);
 		})
-	$('#codcontentwrapper').hover(
+	$('#codiconwrapper').hover(
 		function(){
 			$('#codcloak').fadeTo(300,1);
 			$('#codcontentwrapper h1').animate({'backgroundColor':'#2ecc71'},1000);
 		})
-	$('#liftcontentwrapper').hover(
+	$('#lifticonwrapper').hover(
 		function(){
 			$('#liftcloak').fadeTo(300,1);
 			$('#liftcontentwrapper h1').animate({'backgroundColor':'#f39c12'},1000);
+		})
+	$('#contactbutton').click(
+		function(){
+			$('#contactbox').fadeIn(250);
+		})
+	$('#exitcontact').click(
+		function(){
+			$('#contactbox').fadeOut(250);
+		})
+	$('#imagebutton').click(
+		function(){
+			$('#imagebox').fadeIn(250);
+		})
+	$('#exitcontact2').click(
+		function(){
+			$('#imagebox').fadeOut(250);
 		})
 })
