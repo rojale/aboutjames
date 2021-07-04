12b3ca966a61c3a4425d99f685511813ea785630
rojale <j08zhang24@gmail.com>
2021-07-04 00:10:01 -0400

    windows horizontalScrollbar

--- horizontalScrollBar.js
+++ horizontalScrollBar.js
@@ -1,11 +1,12 @@
 const checkForScrollBar = () => {
-    const body = document.getElementById("IndexBody");
-    console.log(body.scrollHeight, body.clientHeight);
+    const body = getMainBody();
 
     const bodyIsOverflowing = checkBodyOverflow(body);
 
     conditionallyApplyOverflowShrink(bodyIsOverflowing);
 };
+
+const getMainBody = () => document.getElementById("MainBody");
 
 const checkBodyOverflow = (bodyElement) =>
     bodyElement.scrollHeight > window.innerHeight;
@@ -22,18 +23,41 @@ const functionIf = (trueCb, falseCb, bool) => {
 };
 
 const applyShrink = () => {
-    console.log('apply')
+    const scrollBarWidth = getScrollbarWidth();
+    const bodyElement = getMainBody()
+    bodyElement.style.width = `calc(100vw - ${scrollBarWidth}px)`
+};
 
-}
+const removeShrink = () => {
+    const bodyElement = getMainBody()
+    bodyElement.style.width = "100vw"
+};
 
-const removeShrink = () => { console.log('remove') }
+const getScrollbarWidth = () => {
+    const outer = document.createElement("div");
+    outer.style.visibility = "hidden";
+    outer.style.overflow = "scroll"; // forcing scrollbar to appear
+    outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+    document.body.appendChild(outer);
+
+    // Creating inner element and placing it in the container
+    const inner = document.createElement("div");
+    outer.appendChild(inner);
+
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+    // Removing temporary elements from the DOM
+    outer.parentNode.removeChild(outer);
+
+    return scrollbarWidth;
+};
 
 const conditionallyApplyOverflowShrink = partialApply(
     functionIf,
     applyShrink,
     removeShrink
 );
-
 
 const throttledCheckForScrollBar = () => {
     window.requestAnimationFrame(checkForScrollBar);
